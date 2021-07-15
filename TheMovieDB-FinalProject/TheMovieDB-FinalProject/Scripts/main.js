4684f4c5aa0823097243e47d9a625374309bafe9
MagenR <83386524+MagenR@users.noreply.github.com>
2021-07-15 09:50:37 +0300

    Major update to series and main pages.
    Series and Main pages were revamped regarding navbar.
    Owl carousel added to series page actors section.
    seasons episodes got modals.
    fixed bugs where seasons would not generate episodes correctly.

--- TheMovieDB-FinalProject/TheMovieDB-FinalProject/Scripts/main.js
+++ TheMovieDB-FinalProject/TheMovieDB-FinalProject/Scripts/main.js
@@ -10,10 +10,7 @@ maxResults = 6;
 $(document).ready(function () {
 
     $('#searchTvBtn').click(getSearchResults);
-    $('#accessUserBtn').click(getUser);
-    $('#logOutBtn').click(logOut);
-    $('#signUpModal').submit(signUp);
-    $("#passwordTB").on("blur", validatePassword);
+
     $('#ph').on('click', '.personPicture', function () {
         $targetId = $(this).data('personid');
         window.location.href = "person.html?id=" + $targetId;
@@ -22,10 +19,6 @@ $(document).ready(function () {
         $targetId = $(this).data('seriesid');
         window.location.href = "series.html?id=" + $targetId;
     });
-    /*.tvPoster: hover,*/
-
-    setMaxDate();
-    loadSavedLogIn();
 
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -52,61 +45,6 @@ $(document).ready(function () {
 // ---------------------------------------- API calls ---------------------------------------------
 
 {
-
-    // Users ---------------------------------------------------------------------------------
-
-    {
-
-        //--------------------------------------- GET -------------------------------------
-
-        function getUser() {
-            let api = "../api/Users?email=" + $('#emailLogInTB').val() + "&password=" + $('#passwordLogInTB').val();
-            ajaxCall("GET", api, "", getUserSuccessCB, getUserErrorCB);
-        }
-
-        function getUserSuccessCB(user) {
-            logIn(user);
-        }
-
-        function getUserErrorCB(err) {
-            console.log(err.status + " " + err.responseJSON.Message);
-            if (err.status == '404')
-                swal("Error!", "404: " + err.responseJSON.Message, "error");
-            else
-                swal("Error!", err.responseJSON.Message, "error");
-        }
-
-        //--------------------------------------- POST ------------------------------------
-
-        function postUser() {
-
-            let user = {
-                First_name: $('#firstNameTB').val(),
-                Last_name: $('#lastNameTB').val(),
-                Email: $('#emailAddressTB').val(),
-                Password: $('#passwordTB').val(),
-                Phone_num: $('#phoneNumTB').val(),
-                Address: $('#addressTB').val(),
-                Fav_genre: $('#favGenreTB').val(),
-                Gender: $('#genderDDL').val(),
-                Birth_date: $('#birthYearTB').val()
-            }
-
-            let api = "../api/Users";
-            ajaxCall("POST", api, JSON.stringify(user), postUserSuccessCB, postUserErrorCB);
-        }
-
-        function postUserSuccessCB(msg) {
-            $('#signUpModal').modal('hide');
-            swal("Success!", msg, "success");
-        }
-
-        function postUserErrorCB(err) {
-            console.log(err.status + " " + err.responseJSON.Message);
-            swal("Error!", err.responseJSON.Message, "error");
-        }
-
-    } // End of 'Users.
 
     // Trending ------------------------------------------------------------------------------
 
@@ -183,6 +121,9 @@ $(document).ready(function () {
     //  Actors/People ------------------------------------------------------------------------
 
     {
+
+        //--------------------------------------- GET -------------------------------------
+
         function getActors() {
             let apiCall = url + "3/person/popular?api_key=" + key + "&page=1";
             ajaxCall("GET", apiCall, "", getPeopleSuccessCB, getErrorCB)
@@ -230,22 +171,6 @@ $(document).ready(function () {
 
 {
 
-    function setMaxDate() {
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth() + 1; //January is 0!
-        var yyyy = today.getFullYear();
-        if (dd < 10) {
-            dd = '0' + dd
-        }
-        if (mm < 10) {
-            mm = '0' + mm
-        }
-
-        today = yyyy + '-' + mm + '-' + dd;
-        document.getElementById("birthYearTB").setAttribute("max", today);
-    }
-
     // ---------------------------------------- Renders --------------------------------------
 
     function renderPosters(location, source, totShow) {
@@ -286,66 +211,6 @@ $(document).ready(function () {
                 + '<h5>' + source.results[i].name + '<h5>'
                 + '</div>');
         }
-    }
-
-    // ---------------------------------------- User commands --------------------------------
-
-    function validatePassword() {
-
-        let upper = /[A-Z]/;
-        let number = /\d/;
-
-        if (this.value.length < 6) {
-            this.validity.valid = false;
-            this.setCustomValidity("Please make sure password is atleast 6 characters.");
-            return;
-        }
-        if (!number.test(this.value)) {
-            this.validity.valid = false;
-            this.setCustomValidity("Please make sure Password Includes a Digit");
-            return;
-        }
-        if (!upper.test(this.value)) {
-            this.validity.valid = false;
-            this.setCustomValidity("Please make sure password includes an uppercase letter.");
-            return;
-        }
-        else {
-            this.validity.valid = true;
-            this.setCustomValidity('');
-        }
-    }
-
-    function signUp() {
-        postUser();
-        return false;
-    }
-
-    function loadSavedLogIn() {
-        logged_user = JSON.parse(localStorage.getItem('user'));
-        if (logged_user != null)
-            logIn(logged_user);
-        else
-            $("#logOutBtn").hide();
-    }
-
-    function logIn(user) {
-        delete user.Password; // Remove password before saving to LS.
-        localStorage.setItem('user', JSON.stringify(user));
-        $("#WlcmMsg").html("Hello, " + user.First_name + " " + user.Last_name + ".");
-        $("#logOutBtn").show();
-        $("#signUpBtn").hide();
-        $("#logInBtn").hide();
-        $('#logInModal').modal('hide');
-    }
-
-    function logOut() {
-        $("#WlcmMsg").html("");
-        $("#logOutBtn").hide();
-        $("#signUpBtn").show();
-        $("#logInBtn").show();
-        localStorage.clear();
-        location.reload();
     }
 
 } // End of 'Functions'.
